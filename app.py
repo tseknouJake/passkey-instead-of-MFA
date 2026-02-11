8079b0d3a2a8aace0b4963eb07a2d09bfd9f90f4
Leah Goldin <lejanskas@gmail.com>
2026-02-11 13:46:19 +0100

    opening port 0000

--- app.py
+++ app.py
@@ -352,4 +352,4 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, ssl_context='adhoc')
+    app.run(debug=True, port=5000, ssl_context='adhoc', host="0.0.0.0")
